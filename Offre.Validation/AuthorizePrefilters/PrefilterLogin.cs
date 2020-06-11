651ff5923ae939cc734bb81ddec0a56c61866756
Arkadiusz Kina <arkes987@gmail.com>
2020-06-11 22:40:46 +0200

    #1 - Removed unused email regex

--- Offre.Validation/AuthorizePrefilters/PrefilterLogin.cs
+++ Offre.Validation/AuthorizePrefilters/PrefilterLogin.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Mail;
-using System.Text.RegularExpressions;
 
 namespace Offre.Validation.AuthorizePrefilters
 {
@@ -8,9 +7,6 @@ namespace Offre.Validation.AuthorizePrefilters
     {
         private const int MaxLoginLength = 128;
         private const short MinLoginLength = 8;
-
-        private const string EmailRegex =
-            "\r\n^(?(\")(\".+?(?<!\\\\)\"@)|(([0-9a-z]((\\.(?!\\.))|[-!#\\$%&'\\*\\+/=\\?\\^`\\{\\}\\|~\\w])*)(?<=[0-9a-z])@))(?(\\[)(\\[(\\d{1,3}\\.){3}\\d{1,3}\\])|(([0-9a-z][-\\w]*[0-9a-z]*\\.)+[a-z0-9][\\-a-z0-9]{0,22}[a-z0-9]))$\r\n1\r\n^(?(\")(\".+?(?<!\\\\)\"@)|(([0-9a-z]((\\.(?!\\.))|[-!#\\$%&'\\*\\+/=\\?\\^`\\{\\}\\|~\\w])*)(?<=[0-9a-z])@))(?(\\[)(\\[(\\d{1,3}\\.){3}\\d{1,3}\\])|(([0-9a-z][-\\w]*[0-9a-z]*\\.)+[a-z0-9][\\-a-z0-9]{0,22}[a-z0-9]))$";
         private readonly string _data;
         public PrefilterLogin(string data)
         {
